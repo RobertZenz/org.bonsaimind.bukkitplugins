2442f10b932c1c3e52d2e359469485cc01cce46a
Eric Driggers <admalledd@gmail.com>
2013-02-26 20:04:14 -0800

    I think I have redstone working now, blarg... PS: I hate you eclipse for your autoformatting that mucked up this commit im too lazy to undo by hand and ctrl-z did nothing...

--- RedSponge/src/org/bonsaimind/bukkitplugins/redsponge/Plugin.java
+++ RedSponge/src/org/bonsaimind/bukkitplugins/redsponge/Plugin.java
@@ -3,8 +3,8 @@ package org.bonsaimind.bukkitplugins.redsponge;
 import java.util.logging.Level;
 
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.Material;
-
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,218 +18,174 @@ import org.bukkit.plugin.java.JavaPlugin;
 /**
  * RedSponge for Bukkit
  * 
- * Big list of TODO's:
- * 
- * Update blocks on removal
- * 
- * move around the getConfig() stuff
- *	anything that gets from the config with the same settings 
- *	likely has need of refactoring into sub-functions (see clearwater vs clearlava, too much code dupe)
- * 
- * 
- * 
  * @author admalledd
  */
-public class Plugin extends JavaPlugin implements Listener
-{
+public class Plugin extends JavaPlugin implements Listener {
 	@Override
-	public void onEnable()
-	{
-		getServer().getPluginManager().registerEvents(this,this);
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
 		PluginDescriptionFile pdfFile = this.getDescription();
-		this.getLogger().log(Level.INFO, pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
+		this.getLogger().log(
+				Level.INFO,
+				pdfFile.getName() + " version " + pdfFile.getVersion()
+						+ " is enabled!");
 	}
-	
+
 	@Override
-	public void onDisable()
-	{
+	public void onDisable() {
 		PluginDescriptionFile pdfFile = this.getDescription();
-		
-		this.getLogger().log(Level.INFO, pdfFile.getName() + " version " + pdfFile.getVersion() + " is disabled!");
+
+		this.getLogger().log(
+				Level.INFO,
+				pdfFile.getName() + " version " + pdfFile.getVersion()
+						+ " is disabled!");
 	}
 
 	@EventHandler
-	public void onBlockPlace(BlockPlaceEvent event)
-	{
+	public void onBlockPlace(BlockPlaceEvent event) {
 		Block blockPlaced = event.getBlock();
 		spongeEvent(blockPlaced);
-		
+
 	}
-	
+
 	@EventHandler
-	public void onBlockBreak(BlockBreakEvent event) 
-	{
+	public void onBlockBreak(BlockBreakEvent event) {
 		Block block = event.getBlock();
-		if (block.getType() == Material.SPONGE)
-		{
+		if (block.getType() == Material.SPONGE) {
 			updateBlocks(block);
 		}
 	}
-	
-	
+
 	@EventHandler
-	public void onBlockFlow(BlockFromToEvent event)
-	{
+	public void onBlockFlow(BlockFromToEvent event) {
 		Block blockFrom = event.getBlock();
 		Block blockTo = event.getToBlock();
-		boolean isLava = blockFrom.getType() == Material.LAVA || blockFrom.getType() == Material.STATIONARY_LAVA;
-		boolean isWater = blockFrom.getType() == Material.WATER || blockFrom.getType() == Material.STATIONARY_WATER;
+		//TODO: use isliquid()
+		boolean isLava = blockFrom.getType() == Material.LAVA
+				|| blockFrom.getType() == Material.STATIONARY_LAVA;
+		boolean isWater = blockFrom.getType() == Material.WATER
+				|| blockFrom.getType() == Material.STATIONARY_WATER;
+		
 		boolean waterSponge = getConfig().getBoolean("options.water");
 		boolean lavaSponge = getConfig().getBoolean("options.lava");
 
 		// is the flow water or lava and are the sponges enabled
-		if ((lavaSponge && isLava) || (waterSponge && isWater))
-		{
+		if ((lavaSponge && isLava) || (waterSponge && isWater)) {
 			cancelFlow(blockTo, event);
 		}
 	}
+
 	@EventHandler
-	public void onBlockRedstoneChange(BlockRedstoneEvent event)
-	{
-		if (!getConfig().getBoolean("options.redstone"))			
-		{
-			return; //return early to bypass redstone logics
+	public void onBlockRedstoneChange(BlockRedstoneEvent event) {
+
+		if (!getConfig().getBoolean("options.redstone")) {
+			return; // return early to bypass redstone logics
 		}
-		Block blockTo = event.getBlock();
-		boolean blockWhenPowered = getConfig().getBoolean("invert");
+		Block north = event.getBlock().getRelative(BlockFace.NORTH);
+		Block east = event.getBlock().getRelative(BlockFace.EAST);
+		Block south = event.getBlock().getRelative(BlockFace.SOUTH);
+		Block west = event.getBlock().getRelative(BlockFace.WEST);
+		Block up = event.getBlock().getRelative(BlockFace.UP);
+		Block down = event.getBlock().getRelative(BlockFace.DOWN);
 
-		//check around block to find if any are our sponge
-		for (int radX = -1; radX <= 1; radX++)
-		{
-			for (int radY = -1; radY <= 1; radY++)
+		int newCurrent = event.getNewCurrent();
+		int oldCurrent = event.getOldCurrent();
+		Block sponge = null;
+		if (north.getType() == Material.SPONGE) {
+			sponge = north;
+		} else if (east.getType() == Material.SPONGE) {
+			sponge = east;
+		} else if (south.getType() == Material.SPONGE) {
+			sponge = south;
+		} else if (west.getType() == Material.SPONGE) {
+			sponge = west;
+		} else if (up.getType() == Material.SPONGE) {
+			sponge = up;
+		} else if (down.getType() == Material.SPONGE) {
+			sponge = down;
+		}
+		if (sponge != null) {
+
+			// redstone changes from low to high
+			if (oldCurrent == 0 && newCurrent >= 1)
 			{
-				for (int radZ = -1; radZ <= 1; radZ++)
-				{
-
-					
-					Block isThisSponge = blockTo.getRelative(radX, radY, radZ);
-					
-					if (isThisSponge.getType() == Material.SPONGE)
-					{
-						getLogger().info(String.format("power change, blockcheck: %d,%d,%d",
-								isThisSponge.getX(),isThisSponge.getY(),isThisSponge.getZ()));
-						if (blockWhenPowered == false)//normal sponge (deactivates when powered)
-						{
-							if (!ispowered(isThisSponge))
-							{
-								//now powered, let the water flow!
-								getLogger().info("power off, updated");
-								updateBlocks(isThisSponge);
-	
-							}
-							else if (ispowered(isThisSponge))
-							{
-								getLogger().info("power on, cleared");
-								//no longer powered, clear it all again
-								spongeEvent(isThisSponge);
-							}
-						}
-	
-						else//inverted sponge (clears when powered only)
-						{
-							if (!ispowered(isThisSponge))
-							{
-								//not powered, let the water flow!
-								getLogger().info("power off, updated");
-								updateBlocks(isThisSponge);
-							}
-							else if (ispowered(isThisSponge))
-							{
-								//powered, clear it all again
-								getLogger().info("power on, cleared");
-								spongeEvent(isThisSponge);
-							}
-						}
-					}
-				}
+				getLogger().info("low->high");
+				updateBlocks(sponge);
+			}
+			// redstone changes from high to low
+			else if (oldCurrent >= 1 && newCurrent == 0)
+			{
+				getLogger().info("high->low");
+				clearArea(sponge);
 			}
 		}
-	}	
-	
+	}
 
-	public void spongeEvent(Block blockPlaced)
-	{
-		boolean blockWhenPowered = getConfig().getBoolean("invert");
-		
-		if (blockPlaced.getType() == Material.SPONGE)
-		{
+	public void spongeEvent(Block blockPlaced) {
+		boolean blockWhenPowered = getConfig().getBoolean("options.invert");
+
+		if (blockPlaced.getType() == Material.SPONGE) {
 			// blockWhenPowered == false -> default, a sponge works like a
 			// sponge without redstone current
-			//check here as well in case on block place we are powered
-			if (blockWhenPowered == false)
-			{
-				if (!ispowered(blockPlaced))
-				{
+			// check here as well in case on block place we are powered
+			if (blockWhenPowered == false) {
+				if (!ispowered(blockPlaced)) {
 					clearArea(blockPlaced);
-					
+
 				}
 			}
 			// blockWhenPowered == true -> sponge works like sponge WITH
 			// redstone current
-			else
-			{
-				if (ispowered(blockPlaced))
-				{
+			else {
+				if (ispowered(blockPlaced)) {
 					clearArea(blockPlaced);
 				}
 			}
 		}
 	}
-	public void clearArea(Block center)
-	{
-		getLogger().info(String.format("clearing area around: %d,%d,%d", center.getX(),center.getY(),center.getZ()));
+
+	public void clearArea(Block center) {
+		getLogger().info(
+				String.format("clearing area around: %d,%d,%d", center.getX(),
+						center.getY(), center.getZ()));
 		int radius = getConfig().getInt("options.raduis");
-		for (int radX = 0 - radius; radX <= radius; radX++)
-		{
-			for (int radY = 0 - radius; radY <= radius; radY++)
-			{
-				for (int radZ = 0 - radius; radZ <= radius; radZ++)
-				{
+		for (int radX = 0 - radius; radX <= radius; radX++) {
+			for (int radY = 0 - radius; radY <= radius; radY++) {
+				for (int radZ = 0 - radius; radZ <= radius; radZ++) {
 					Block b = center.getRelative(radX, radY, radZ);
 
-					if (isLiquid(b))
-					{
+					if (isLiquid(b)) {
 						b.setType(Material.AIR);
 					}
 				}
 			}
 		}
-		
+
 		updateBlocks(center);
 	}
 
-
-	public void cancelFlow(Block blockTo, BlockFromToEvent event)
-	{
-		boolean blockWhenPowered = getConfig().getBoolean("invert");
+	public void cancelFlow(Block blockTo, BlockFromToEvent event) {
+		boolean blockWhenPowered = getConfig().getBoolean("options.invert");
 		int radius = getConfig().getInt("options.raduis");
 
-		for (int radX = 0 - radius; radX <= radius; radX++)
-		{
-			for (int radY = 0 - radius; radY <= radius; radY++)
-			{
-				for (int radZ = 0 - radius; radZ <= radius; radZ++)
-				{
-					
+		for (int radX = 0 - radius; radX <= radius; radX++) {
+			for (int radY = 0 - radius; radY <= radius; radY++) {
+				for (int radZ = 0 - radius; radZ <= radius; radZ++) {
+
 					Block isThisSponge = blockTo.getRelative(radX, radY, radZ);
 					Material id = isThisSponge.getType();
-					if (blockWhenPowered == false)
-					{
-						if (id == Material.SPONGE && !ispowered(isThisSponge))
-						{
-							getLogger().info(String.format("power off, flow cancelled sponge/from/to: %d,%d,%d / %d,%d,%d / %d,%d,%d",
-									isThisSponge.getX(),isThisSponge.getY(),isThisSponge.getZ(),
-									event.getBlock().getX(),event.getBlock().getY(),event.getBlock().getZ(),
-									blockTo.getX(),blockTo.getY(),blockTo.getZ()));
+					if (blockWhenPowered == false) {
+						if (id == Material.SPONGE && !ispowered(isThisSponge)) {
+							// getLogger().info(String.format("power off, flow cancelled sponge/from/to: %d,%d,%d / %d,%d,%d / %d,%d,%d",
+							// isThisSponge.getX(),isThisSponge.getY(),isThisSponge.getZ(),
+							// event.getBlock().getX(),event.getBlock().getY(),event.getBlock().getZ(),
+							// blockTo.getX(),blockTo.getY(),blockTo.getZ()));
 							event.setCancelled(true);
 							return;
 						}
-					}
-					else
-					{
-						if (id == Material.SPONGE && ispowered(isThisSponge))
-						{
-							//getLogger().info("power on, flow cancelled");
+					} else {
+						if (id == Material.SPONGE && ispowered(isThisSponge)) {
+							// getLogger().info("power on, flow cancelled");
 							event.setCancelled(true);
 							return;
 						}
@@ -238,59 +194,75 @@ public class Plugin extends JavaPlugin implements Listener
 			}
 		}
 	}
-	
-	//is it a liquid that we care to remove?
-	public boolean isLiquid(Block block)
-	{
+
+	// is it a liquid that we care to remove?
+	public boolean isLiquid(Block block) {
 		boolean waterSponge = getConfig().getBoolean("options.water");
 		boolean lavaSponge = getConfig().getBoolean("options.lava");
-		
-		if (lavaSponge && (block.getType() == Material.STATIONARY_LAVA || block.getType() == Material.LAVA))
-		{
+
+		if (lavaSponge
+				&& (block.getType() == Material.STATIONARY_LAVA || block
+						.getType() == Material.LAVA)) {
 			return true;
 		}
-		if (waterSponge && (block.getType() == Material.STATIONARY_WATER || block.getType() == Material.WATER))
-		{
+		if (waterSponge
+				&& (block.getType() == Material.STATIONARY_WATER || block
+						.getType() == Material.WATER)) {
 			return true;
 		}
-		//default false :(
+		// default false :(
 		return false;
-		
+
 	}
-	public void updateBlocks(Block center)
-	{
+
+	public void updateBlocks(Block center) {
 		int raduis = getConfig().getInt("options.raduis");
-		getLogger().info(String.format("updating area around: %d,%d,%d", center.getX(),center.getY(),center.getZ()));
-		for (int x = -(raduis+1); x <= (raduis+1); x++){
-			for (int y = -(raduis+1); y <= (raduis+1); y++){
-				for (int z = -(raduis+1); z <= (raduis+1); z++) {
-					
-					final Block block = center.getRelative(x,y,z);
-					if (isLiquid(block)){//only update the block if it is water/lava ect
+		// terrible debug code left in:
+		//getLogger().info(
+		//		String.format("updating area around: %d,%d,%d", center.getX(),
+		//				center.getY(), center.getZ()));
+		for (int x = -(raduis + 1); x <= (raduis + 1); x++) {
+			for (int y = -(raduis + 1); y <= (raduis + 1); y++) {
+				for (int z = -(raduis + 1); z <= (raduis + 1); z++) {
+
+					final Block block = center.getRelative(x, y, z);
+					// only update the block if it is water/lava ect
+					if (isLiquid(block)) {
+						// use the original id if all else fails (no need to update)
 						int tempid = block.getTypeId();
-						if (tempid==Material.LAVA.getId() || tempid== Material.STATIONARY_LAVA.getId())
-						{ 
+						if (tempid == Material.LAVA.getId()
+								|| tempid == Material.STATIONARY_LAVA.getId()) {
+							//use "moving" lava so that the server knows to update it
 							tempid = Material.LAVA.getId();
-						}
-						if (tempid==Material.WATER.getId() ||tempid == Material.STATIONARY_WATER.getId())
-						{ 
+							}
+						if (tempid == Material.WATER.getId()
+								|| tempid == Material.STATIONARY_WATER.getId()) {
+							//use "moving" water so that the server knows to update it
 							tempid = Material.WATER.getId();
 						}
-						
-						
+
 						byte tempdata = block.getData();
-						//TODO: for some reason when it is the last block of water only it fails to update?
-						getLogger().info(String.format("updating block / around: %s,%s / %d,%d,%d", tempid,tempdata,block.getX(),block.getY(),block.getZ()));
-						block.setType(Material.AIR);//force block update by changing it!
-						block.setTypeIdAndData(tempid, tempdata, true);//this now sets it to what it was, updates
+						// TODO: for some reason when it is the last block of
+						// water only it fails to update?
+						
+						// terrible debug code left in:
+						// getLogger().info(String.format("updating block / around: %s,%s / %d,%d,%d",
+						// tempid,tempdata,block.getX(),block.getY(),block.getZ()));
+						
+						//removeing the block, so that there is a block change to update with
+						block.setType(Material.AIR);
+
+						//this now sets it to what it was, causing a update
+						block.setTypeIdAndData(tempid, tempdata, true);
 					}
 				}
 			}
 		}
 	}
-	public boolean ispowered(Block tocheck)
-	{
-		return (tocheck.isBlockIndirectlyPowered()||tocheck.isBlockPowered());
+
+	public boolean ispowered(Block tocheck) {
+		return (tocheck.isBlockIndirectlyPowered() || tocheck.isBlockPowered());
+		// return tocheck.isBlockPowered();
 	}
 
 }
