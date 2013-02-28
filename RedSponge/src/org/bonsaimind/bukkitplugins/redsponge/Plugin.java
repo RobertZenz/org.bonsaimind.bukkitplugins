58170704564d0f9713a1dab456a02b6fbb8823dc
Eric Driggers <admalledd@gmail.com>
2013-02-27 19:41:22 -0800

    added getRelative() to do all that pesky forlooping for me.

--- RedSponge/src/org/bonsaimind/bukkitplugins/redsponge/Plugin.java
+++ RedSponge/src/org/bonsaimind/bukkitplugins/redsponge/Plugin.java
@@ -1,6 +1,7 @@
 package org.bonsaimind.bukkitplugins.redsponge;
 
 import java.util.logging.Level;
+import java.util.ArrayList;
 
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -65,7 +66,6 @@ public class Plugin extends JavaPlugin implements Listener {
 				|| blockFrom.getType() == Material.STATIONARY_LAVA;
 		boolean isWater = blockFrom.getType() == Material.WATER
 				|| blockFrom.getType() == Material.STATIONARY_WATER;
-		
 		boolean waterSponge = getConfig().getBoolean("options.water");
 		boolean lavaSponge = getConfig().getBoolean("options.lava");
 
@@ -81,6 +81,11 @@ public class Plugin extends JavaPlugin implements Listener {
 		if (!getConfig().getBoolean("options.redstone")) {
 			return; // return early to bypass redstone logics
 		}
+		int newCurrent = event.getNewCurrent();
+		int oldCurrent = event.getOldCurrent();
+		//left in until I can confirm it works...
+		//minecraft is derping again on this system :/
+		/*
 		Block north = event.getBlock().getRelative(BlockFace.NORTH);
 		Block east = event.getBlock().getRelative(BlockFace.EAST);
 		Block south = event.getBlock().getRelative(BlockFace.SOUTH);
@@ -88,8 +93,6 @@ public class Plugin extends JavaPlugin implements Listener {
 		Block up = event.getBlock().getRelative(BlockFace.UP);
 		Block down = event.getBlock().getRelative(BlockFace.DOWN);
 
-		int newCurrent = event.getNewCurrent();
-		int oldCurrent = event.getOldCurrent();
 		Block sponge = null;
 		if (north.getType() == Material.SPONGE) {
 			sponge = north;
@@ -104,16 +107,17 @@ public class Plugin extends JavaPlugin implements Listener {
 		} else if (down.getType() == Material.SPONGE) {
 			sponge = down;
 		}
-		if (sponge != null) {
+		*/
+		for(Block sponge : getRelative(event.getBlock(),1)) {
 
 			// redstone changes from low to high
-			if (oldCurrent == 0 && newCurrent >= 1)
+			if (sponge.getType() == Material.SPONGE && oldCurrent == 0 && newCurrent >= 1)
 			{
 				getLogger().info("low->high");
 				updateBlocks(sponge);
 			}
 			// redstone changes from high to low
-			else if (oldCurrent >= 1 && newCurrent == 0)
+			else if (sponge.getType() == Material.SPONGE && oldCurrent >= 1 && newCurrent == 0)
 			{
 				getLogger().info("high->low");
 				clearArea(sponge);
@@ -149,18 +153,12 @@ public class Plugin extends JavaPlugin implements Listener {
 				String.format("clearing area around: %d,%d,%d", center.getX(),
 						center.getY(), center.getZ()));
 		int radius = getConfig().getInt("options.radius");
-		for (int radX = 0 - radius; radX <= radius; radX++) {
-			for (int radY = 0 - radius; radY <= radius; radY++) {
-				for (int radZ = 0 - radius; radZ <= radius; radZ++) {
-					Block b = center.getRelative(radX, radY, radZ);
-
-					if (isLiquid(b)) {
-						b.setType(Material.AIR);
-					}
-				}
+		for (Block b : getRelative(center,radius)){
+			if (isLiquid(b)) {
+				b.setType(Material.AIR);
 			}
 		}
-
+		//After clearing we want to cause flow updates and such in the area
 		updateBlocks(center);
 	}
 
@@ -168,32 +166,28 @@ public class Plugin extends JavaPlugin implements Listener {
 		boolean blockWhenPowered = getConfig().getBoolean("options.invert");
 		int radius = getConfig().getInt("options.radius");
 
-		for (int radX = 0 - radius; radX <= radius; radX++) {
-			for (int radY = 0 - radius; radY <= radius; radY++) {
-				for (int radZ = 0 - radius; radZ <= radius; radZ++) {
-
-					Block isThisSponge = blockTo.getRelative(radX, radY, radZ);
-					Material id = isThisSponge.getType();
-					if (blockWhenPowered == false) {
-						if (id == Material.SPONGE && !ispowered(isThisSponge)) {
-							// getLogger().info(String.format("power off, flow cancelled sponge/from/to: %d,%d,%d / %d,%d,%d / %d,%d,%d",
-							// isThisSponge.getX(),isThisSponge.getY(),isThisSponge.getZ(),
-							// event.getBlock().getX(),event.getBlock().getY(),event.getBlock().getZ(),
-							// blockTo.getX(),blockTo.getY(),blockTo.getZ()));
-							event.setCancelled(true);
-							return;
-						}
-					} else {
-						if (id == Material.SPONGE && ispowered(isThisSponge)) {
-							// getLogger().info("power on, flow cancelled");
-							event.setCancelled(true);
-							return;
-						}
-					}
+		for (Block isThisSponge : getRelative(blockTo,radius)){
+			Material id = isThisSponge.getType();
+			if (blockWhenPowered == false) {
+				if (id == Material.SPONGE && !ispowered(isThisSponge)) {
+					// getLogger().info(String.format("power off, flow cancelled sponge/from/to: %d,%d,%d / %d,%d,%d / %d,%d,%d",
+					// isThisSponge.getX(),isThisSponge.getY(),isThisSponge.getZ(),
+					// event.getBlock().getX(),event.getBlock().getY(),event.getBlock().getZ(),
+					// blockTo.getX(),blockTo.getY(),blockTo.getZ()));
+					event.setCancelled(true);
+					return;
+				}
+			} else {
+				if (id == Material.SPONGE && ispowered(isThisSponge)) {
+					// getLogger().info("power on, flow cancelled");
+					event.setCancelled(true);
+					return;
 				}
 			}
 		}
 	}
+
+
 
 	// is it a liquid that we care to remove?
 	public boolean isLiquid(Block block) {
@@ -201,13 +195,13 @@ public class Plugin extends JavaPlugin implements Listener {
 		boolean lavaSponge = getConfig().getBoolean("options.lava");
 
 		if (lavaSponge
-				&& (block.getType() == Material.STATIONARY_LAVA || block
-						.getType() == Material.LAVA)) {
+				&& (block.getType() == Material.STATIONARY_LAVA
+					|| block.getType() == Material.LAVA)) {
 			return true;
 		}
 		if (waterSponge
-				&& (block.getType() == Material.STATIONARY_WATER || block
-						.getType() == Material.WATER)) {
+				&& (block.getType() == Material.STATIONARY_WATER 
+					|| block.getType() == Material.WATER)) {
 			return true;
 		}
 		// default false :(
@@ -221,41 +215,33 @@ public class Plugin extends JavaPlugin implements Listener {
 		//getLogger().info(
 		//		String.format("updating area around: %d,%d,%d", center.getX(),
 		//				center.getY(), center.getZ()));
-		for (int x = -(radius + 1); x <= (radius + 1); x++) {
-			for (int y = -(radius + 1); y <= (radius + 1); y++) {
-				for (int z = -(radius + 1); z <= (radius + 1); z++) {
-
-					final Block block = center.getRelative(x, y, z);
-					// only update the block if it is water/lava ect
-					if (isLiquid(block)) {
-						// use the original id if all else fails (no need to update)
-						int tempid = block.getTypeId();
-						if (tempid == Material.LAVA.getId()
-								|| tempid == Material.STATIONARY_LAVA.getId()) {
-							//use "moving" lava so that the server knows to update it
-							tempid = Material.LAVA.getId();
-							}
-						if (tempid == Material.WATER.getId()
-								|| tempid == Material.STATIONARY_WATER.getId()) {
-							//use "moving" water so that the server knows to update it
-							tempid = Material.WATER.getId();
-						}
-
-						byte tempdata = block.getData();
-						// TODO: for some reason when it is the last block of
-						// water only it fails to update?
-						
-						// terrible debug code left in:
-						// getLogger().info(String.format("updating block / around: %s,%s / %d,%d,%d",
-						// tempid,tempdata,block.getX(),block.getY(),block.getZ()));
-						
-						//removeing the block, so that there is a block change to update with
-						block.setType(Material.AIR);
-
-						//this now sets it to what it was, causing a update
-						block.setTypeIdAndData(tempid, tempdata, true);
+		for (Block block : getRelative(center,radius+1)){
+			// only update the block if it is water/lava ect
+			if (isLiquid(block)) {
+				// use the original id if all else fails (no need to update)
+				int tempid = block.getTypeId();
+				if (tempid == Material.LAVA.getId()
+						|| tempid == Material.STATIONARY_LAVA.getId()) {
+					//use "moving" lava so that the server knows to update it
+					tempid = Material.LAVA.getId();
 					}
+				if (tempid == Material.WATER.getId()
+						|| tempid == Material.STATIONARY_WATER.getId()) {
+					//use "moving" water so that the server knows to update it
+					tempid = Material.WATER.getId();
 				}
+					byte tempdata = block.getData();
+				// TODO: for some reason when it is the last block of
+				// water only it fails to update?
+
+				// terrible debug code left in:
+				// getLogger().info(String.format("updating block / around: %s,%s / %d,%d,%d",
+				// tempid,tempdata,block.getX(),block.getY(),block.getZ()));
+
+				//removeing the block, so that there is a block change to update with
+				block.setType(Material.AIR);
+					//this now sets it to what it was, causing a update
+				block.setTypeIdAndData(tempid, tempdata, true);
 			}
 		}
 	}
@@ -264,5 +250,15 @@ public class Plugin extends JavaPlugin implements Listener {
 		return (tocheck.isBlockIndirectlyPowered() || tocheck.isBlockPowered());
 		// return tocheck.isBlockPowered();
 	}
-
+	public ArrayList<Block> getRelative(Block center,int radius){
+		ArrayList<Block> blocks = new ArrayList<Block>();
+		for (int x = -(radius); x <= (radius); x++) {
+			for (int y = -(radius); y <= (radius); y++) {
+				for (int z = -(radius); z <= (radius); z++) {
+					blocks.add(center.getRelative(x,y,z));
+				}
+			}
+		}
+		return blocks;
+	}
 }
