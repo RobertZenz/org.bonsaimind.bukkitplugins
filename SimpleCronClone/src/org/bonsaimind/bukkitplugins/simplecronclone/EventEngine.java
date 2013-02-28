b121ee423bace844a010469bd4d311a2a0fa4337
Robert Zenz <Bobby@bonsaimind.org>
2013-02-28 23:13:29 +0100

    I should really stop using production code for testing...

--- SimpleCronClone/src/org/bonsaimind/bukkitplugins/simplecronclone/EventEngine.java
+++ SimpleCronClone/src/org/bonsaimind/bukkitplugins/simplecronclone/EventEngine.java
@@ -20,9 +20,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
